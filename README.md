5cd10a4d85c1e50603f679372e2dff4daad78090
Kirabou <kirabou@gmx.com>
2018-10-31 09:01:17 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,63 @@
+# dumpDER.go - dump a DER file, displaying its structure and content
+
+## Description
+`dumpDER` is an example of using the `encoding/asn1` package to read and parse a DER file from `stdin`. It will display the DER file structure on `stdio` as well as the data it included, trying to improve the information displayed by converting the most current types (time, string, etc.) in a readable way and retrieving the name of object identifiers from the object identifier repository.
+
+## Usage
+
+Use the `dumpDER` command providing a DER file as its standard input. Example :
+
+```
+dumpDER < file.der
+```
+I tried it with smime.p7s files, as well as cert, request and keypair files. I took my test files from [X509 certificate examples for testing and verification](http://fm4dd.com/openssl/certexamples.htm)
+
+```
+$ ./dumpDER </512b-rsa-example-request.der
+Note: all INTEGER, OCTET STRING and BIT STRING values displayed as hexadecimal bytes
+SEQUENCE (260 bytes)                            :
+| SEQUENCE (175 bytes)                          :
+| | INTEGER (1 bytes)                           : 00
+| | SEQUENCE (74 bytes)                         :
+| | | SET (11 bytes)                            :
+| | | | SEQUENCE (9 bytes)                      :
+| | | | | OBJECT IDENTIFIER (3 bytes)           : 2.5.4.6 attributeType(4) countryName(6)
+| | | | | PRINTABLE STRING (2 bytes)            : JP
+| | | SET (14 bytes)                            :
+| | | | SEQUENCE (12 bytes)                     :
+| | | | | OBJECT IDENTIFIER (3 bytes)           : 2.5.4.8 attributeType(4) stateOrProvinceName(8)
+| | | | | UTF8 STRING (5 bytes)                 : Tokyo
+| | | SET (17 bytes)                            :
+| | | | SEQUENCE (15 bytes)                     :
+| | | | | OBJECT IDENTIFIER (3 bytes)           : 2.5.4.10 attributeType(4) organizationName(10)
+| | | | | UTF8 STRING (8 bytes)                 : Frank4DD
+| | | SET (24 bytes)                            :
+| | | | SEQUENCE (22 bytes)                     :
+| | | | | OBJECT IDENTIFIER (3 bytes)           : 2.5.4.3 attributeType(4) commonName(3)
+| | | | | UTF8 STRING (15 bytes)                : www.example.com
+| | SEQUENCE (92 bytes)                         :
+| | | SEQUENCE (13 bytes)                       :
+| | | | OBJECT IDENTIFIER (9 bytes)             : 1.2.840.113549.1.1.1 pkcs-1(1) rsaEncryption(1)
+| | | | NULL (0 bytes)                          : NUL
+| | | BIT STRING (75 bytes)                     : 00 30 48 02 41 00 9B FC 66 90 79 84 42 BB AB 13
+| | | |                                           FD 2B 7B F8 DE 15 12 E5 F1 93 E3 06 8A 7B B8 B1
+| | | |                                           E1 9E 26 BB 95 01 BF E7 30 ED 64 85 02 DD 15 69
+| | | |                                           A8 34 B0 06 EC 3F 35 3C 1E 1B 2B 8F FA 8F 00 1B
+| | | |                                           DF 07 C6 AC 53 07 02 03 01 00 01
+| | 0 (0 bytes)                                 :
+| SEQUENCE (13 bytes)                           :
+| | OBJECT IDENTIFIER (9 bytes)                 : 1.2.840.113549.1.1.5 pkcs-1(1) sha1-with-rsa-signature(5)
+| | NULL (0 bytes)                              : NUL
+| BIT STRING (65 bytes)                         : 00 72 39 5E 76 63 5E F2 F3 1C 35 57 FC 6F AE ED
+| |                                               EB 2C FA D5 C5 80 17 4B 94 A0 BC DA 5F 06 C8 F7
+| |                                               F2 53 55 B5 3B EE 1F F3 20 AE 80 60 9A 34 A9 9E
+| |                                               A2 AA 06 20 43 92 86 36 61 41 13 DA A9 86 8C 0B
+| |                                               BD
+```
+
+
+## Known limitations
+
+1. The `encoding/asn1`package only implements a subset of ASN.1. Not all ASN.1 types are recognized. The list of the ASN.1 types than can be recognized by the `encoding/asn1` package are described in the [Unmarshal function documentation](https://golang.org/pkg/encoding/asn1/#Unmarshal)
+2. To retrieve the name of the ASN.1 object identifier I am using a GET from the [OID Repository Web site](http://oid-info.com/). It's not very efficient, especially for DER file with a lot of objects, and should be optimized.
+3. Some objects use OCTET STRING or BIT STRING as extension for other ASN.1 data, as described [here](https://stackoverflow.com/questions/15299201/asn-1-octet-strings). These extensions are currently not parsed as ASN.1 data and only displayed as hexadecimal bytes.
---
+++ dumpDER.go
@@ -0,0 +1,301 @@
+package main
+
+import (
+	// "bytes"
+	// "encoding/base64"
+	"encoding/asn1"
+	"fmt"
+	"golang.org/x/net/html"
+	// "io"
+	"io/ioutil"
+	"log"
+	// "mime"
+	// "mime/multipart"
+	// "mime/quotedprintable"
+	"net/http"
+	// "net/mail"
+	"os"
+	// "reflect"
+	"strings"
+	"time"
+)
+
+
+
+// WidthFieldNameColumn is the width of the left column used to display 
+// data field name. You can change it to any value you like
+var WidthFieldNameColumn int = 48
+
+
+
+// GetHtmlTitle return the title of a DOM tree, or an empty string
+func GetHtmlTitle(n *html.Node) string {
+
+	if n.Type == html.ElementNode && n.Data == "title" {
+		return n.FirstChild.Data
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+        s := GetHtmlTitle(c)
+        if len(s) > 0 {
+        	return s
+        }
+    }
+
+    return ""
+}
+
+
+// GetOIName retrieve name of an object identifier from the oid-info.com Web site.
+// Only the last two parts of the are kept to avoid name to be too long and
+// difficult to read.
+func GetOIName(oi string) string {
+	
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://oid-info.com/get/%s", oi), nil)
+	if err != nil {
+		log.Println("Cannot build request for oid-info failed -", err)
+		return ""
+	}
+
+	// We need to provide a decent user agent to access oid-info.com
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("Get from oid-info failed -", err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		log.Println("HTML parse from oid-info failed -", err)
+		return ""
+	}
+
+	s := GetHtmlTitle(doc)
+
+	// We need to split the title to keep the last 2 parts
+	f := func(c rune) bool {
+		return c == ' ' || c == '{' || c == '}'
+	}
+	split := strings.FieldsFunc(s, f)
+	
+	switch l := len(split); l {
+	case 0:
+		return("")
+	case 1:
+		return fmt.Sprint("%s", split[0])
+	default:
+		return fmt.Sprintf("%s %s", split[l-2], split[l-1])
+	}
+
+}
+
+
+// PrintHex dump a byte slide as hexadecimal, with width and left margin
+// of the dump given as parameters
+func PrintHex(data []byte, prefix string, width int, margin int) {
+
+	if len(data) == 0 {
+		fmt.Printf("NUL")
+		return
+	}
+
+	var i int
+	var b byte
+	for i, b = range data {
+		switch i % width {
+			case 0:	
+				if i == 0 {
+					fmt.Printf("%02X", b)
+				} else {
+					// fmt.Printf(" %s%02X", strings.Repeat(" ", margin), b)
+					fmt.Printf("%s|%s%02X", prefix, strings.Repeat(" ", margin-len(prefix)), b)
+				}
+			case 1:
+				fmt.Printf(" %02X", b)
+			case width - 1:
+				if i == len(data)-1 {
+					fmt.Printf(" %02X", b)
+				} else {
+					fmt.Printf(" %02X\n", b)
+				}
+			default:
+				fmt.Printf(" %02X", b)
+		}
+	}
+
+
+}
+
+
+
+// GetStringFromTag match an ASN1 tag with a name returned as a string
+func GetStringFromTag(tag int) string {
+
+	switch tag {
+        case asn1.TagBoolean        : return("BOOLEAN")
+        case asn1.TagInteger        : return("INTEGER")
+        case asn1.TagBitString      : return("BIT STRING")
+        case asn1.TagOctetString    : return("OCTET STRING")
+        case asn1.TagNull           : return("NULL")
+        case asn1.TagOID            : return("OBJECT IDENTIFIER")
+        case asn1.TagEnum           : return("ENUM")
+        case asn1.TagUTF8String     : return("UTF8 STRING")
+        case asn1.TagSequence       : return("SEQUENCE")
+        case asn1.TagSet            : return("SET")
+        case asn1.TagNumericString  : return("NUMERIC STRING")
+        case asn1.TagPrintableString: return("PRINTABLE STRING")
+        case asn1.TagT61String      : return("T61String")
+        case asn1.TagIA5String      : return("IA5String")
+        case asn1.TagUTCTime        : return("UTCTime")
+        case asn1.TagGeneralizedTime: return("GeneralizedTime")
+        case asn1.TagGeneralString  : return("GENERAL STRING")
+	}
+
+	return fmt.Sprintf("%d", tag)
+
+}
+
+
+// GetAsnValueAsString convert the raw value of parsed ASN1 data as
+// something more readable depending of the ASN1 tage value of the
+// data read
+func GetAsnValueAsString(asn *asn1.RawValue) string {
+
+	if asn == nil {
+		return ""
+	}
+
+	switch asn.Tag {
+	case asn1.TagOID:
+		var oi asn1.ObjectIdentifier
+		_, err := asn1.Unmarshal(asn.FullBytes, &oi)
+		if err != nil {
+			log.Fatalln("Erreur unmarshalling -", err)
+		}
+		s := oi.String()
+		return fmt.Sprintf("%s %s", s, GetOIName(s))
+	case asn1. TagPrintableString, asn1.TagIA5String, asn1.TagNumericString, asn1.TagUTF8String:
+		var asn_string string
+		_, err := asn1.Unmarshal(asn.FullBytes, &asn_string)
+		if err != nil {
+			log.Fatalln("Erreur unmarshalling -", err)
+		}
+		return asn_string
+	case asn1.TagUTCTime, asn1.TagGeneralizedTime:
+		var t time.Time
+		_, err := asn1.Unmarshal(asn.FullBytes, &t)
+		if err != nil {
+			log.Fatalln("Erreur unmarshalling -", err)
+		}
+		return t.String()
+	case asn1.TagBoolean:
+		var b bool
+		_, err := asn1.Unmarshal(asn.FullBytes, &b)
+		if err != nil {
+			log.Fatalln("Erreur unmarshalling -", err)
+		}
+		if b {
+			return ("true")
+		} else {
+			return("false")
+		}
+
+	}
+
+	return ""
+
+}
+
+
+
+// PrintFieldName print the name of an ASN1 data field, making sure to
+// stay in the WidthFieldNameColumn defined
+func PrintFieldName(s string) {
+	fmt.Printf("\n%-*.*s: ", WidthFieldNameColumn, WidthFieldNameColumn, s)
+}
+
+
+
+// Parse parse a slice of bytes as ASN1 data. It runs recursively to manage
+// nested data enty
+func Parse(data []byte, index int) {
+
+	if len(data) == 0 {
+		return
+	}
+
+	var asn asn1.RawValue
+	var data2 []byte = data
+
+	for {
+
+		rest, err := asn1.Unmarshal(data2, &asn)
+		if err != nil {
+			log.Printf("Erreur unmarshalling - %v\n", err)
+			log.Printf("\n% X\n", data)
+			break
+		}
+
+		// log.Printf("[%02d] %s (%d bytes) IsCompound:%v Rest:%d", index, GetStringFromTag(asn.Tag), len(asn.Bytes), asn.IsCompound, len(rest))
+		PrintFieldName(fmt.Sprintf("%s%s (%d bytes)", strings.Repeat("| ", index-1), GetStringFromTag(asn.Tag), len(asn.Bytes)))
+
+		if asn.IsCompound {
+			Parse(asn.Bytes, index+1)
+		}
+
+		if rest == nil {
+			fmt.Println("-- no more rest")
+			break
+		}
+
+		if !asn.IsCompound {
+			s := GetAsnValueAsString(&asn)
+			if len(s) == 0 {
+				PrintHex(asn.Bytes, fmt.Sprintf("%s", strings.Repeat("| ", index-1)), 16, WidthFieldNameColumn+1)
+			} else {
+				fmt.Printf("%s", s)
+			}
+		}
+
+		// log.Printf("[%02d] %s (%d bytes) IsCompound:%v Rest:%d", index, GetStringFromTag(asn.Tag), len(asn.Bytes), asn.IsCompound, len(rest))
+
+		// Stop when no rest left, or rest is a line feed character
+		if len(rest) == 0 || (len(rest) == 1 && rest[0] == '\x0a') {
+			break
+		} else {
+			data2 = rest
+		}
+
+	}
+
+
+}
+
+
+
+
+// dumpDER is a Go program to read a DER file from stdin and display its structure and content
+// in a readable way on stdio. Based on the Golang encoding/asn1 package to parse the DER file
+func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Read DER file from Stdin
+	der, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalln("Erreur lecture stdin -", err)		
+	}
+
+	fmt.Printf("Note: all INTEGER, OCTET STRING and BIT STRING values displayed as hexadecimal bytes")
+
+	Parse(der, 1)
+
+	fmt.Println()
+
+}
+
+
+
